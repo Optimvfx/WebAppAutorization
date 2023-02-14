abe3e2fcc6613b53a22f3937356fb5d02819768b
Maxim Sidorenko <94392000+Optimvfx@users.noreply.github.com>
2023-02-14 11:26:10 +0200

    Initial commit

---
+++ WebAppAutorization/Data/Identity/ApplicationIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace WebAppAuthorization.Data.Identity
+{
+    public class ApplicationIdentityUser : IdentityUser
+    {
+        public string City { get; set; }
+    }
+}
